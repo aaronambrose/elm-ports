967644b56a542b0bbe9e92f94080820f9d05cdf7
Aaron Ambrose <aaron.ambrose@gmail.com>
2016-12-13 10:25:55 -0800

    added selected state to navigation

--- src/Main/View.elm
+++ src/Main/View.elm
@@ -2,6 +2,7 @@ module Main.View exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
+import Html.Attributes exposing (class)
 import Main.Models exposing (Model)
 import Main.Types exposing (Msg(..), Route(..))
 import Pages.Home as Home
@@ -38,18 +39,26 @@ page model =
             notFoundView
 
 
-navButton : String -> String -> Html Msg
-navButton route name =
-    li [ onClick (ChangePage route) ] [ text name ]
+navButton : Model -> String -> String -> Route -> Html Msg
+navButton model path name route =
+    li [ onClick <| ChangePage path, class (routeToStyle model route) ] [ text name ]
+
+
+routeToStyle : Model -> Route -> String
+routeToStyle model route =
+    if model.route == route then
+        "button selected"
+    else
+        "button"
 
 
 pageHeader : Model -> Html Msg
 pageHeader model =
     header []
         [ ul []
-            [ navButton "/home" "Home"
-            , navButton "/about" "About"
-            , navButton "/details" "Details"
+            [ navButton model "/home" "Home" HomeRoute
+            , navButton model "/about" "About" AboutRoute
+            , navButton model "/details" "Details" DetailsRoute
             ]
         ]
 
--- src/Native/native.js
+++ src/Native/native.js
@@ -1,7 +1,6 @@
 import {Chart} from './chart';
 import * as d3 from "d3";
 
-
 export const NativeApp = (app) => {
   app.ports.initChart.subscribe(function([divId, data]) {
     const chart = Chart(divId, data);
